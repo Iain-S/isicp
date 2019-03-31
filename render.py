28d30a0011b5354ac14651c50e313da0499b25ce
iain <iain.stenson1@gmail.com>
2019-03-31 16:01:13 +0100

    Added render_all.py, which required some changes to render.py.  render_all.py does the same as render_all.sh but is in python, making it convenient for Windows users.

--- render.py
+++ render.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python
-
-import sys, re
+from __future__ import print_function
+import sys
+import re
 
 tag_re = re.compile(
-  r"^({{[\w@]+}})$"  # ()Capture {{Tag}} word,
-  r"(.*?)"  # then ()capture characters until
-  r"^@@$",  # terminating "@@" line.
-  flags=re.DOTALL|re.MULTILINE)  # '.' includes '\n' | '^' and '$' are per line.
+    r"^({{[\w@]+}})$"  # ()Capture {{Tag}} word,
+    r"(.*?)"  # then ()capture characters until
+    r"^@@$",  # terminating "@@" line.
+    flags=re.DOTALL | re.MULTILINE)  # '.' includes '\n' | '^' and '$' are per line.
 
-with open(sys.argv[1]) as content:
-  tag_content = dict(re.findall(tag_re, ''.join(content.readlines())))
 
-with open("template.html") as template:
-  for L in template.readlines():
-    print tag_content.get(L.strip(), L)
+def get_tagged_content(file_path):
+    with open(file_path, mode="r", encoding="utf8") as content:
+        tag_content = dict(re.findall(tag_re, ''.join(content.readlines())))
+        return tag_content
+
+
+def write_output(tagged_content, output_function):
+    with open("template.html", mode="r", encoding="utf8") as template:
+        for line in template.readlines():
+            output_function(tagged_content.get(line.strip(), line))
+
+
+if __name__ == "__main__":
+    write_output(get_tagged_content(sys.argv[1]), print)
---
+++ render_all.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import os
+from render import get_tagged_content, write_output
+
+
+if __name__ == "__main__":
+    for input_filename in os.listdir(path="content"):
+        with open(input_filename.replace(".content", ""), mode="w", encoding="utf8") as output_file:
+            def file_writer(line):
+                output_file.write(line + "\n")
+
+            write_output(get_tagged_content(os.path.join("content", input_filename)), file_writer)
